9ef68c25852ec9017bafe4d9b6991abc3e75e7fe
ivannikova <ivannikova93@gmail.com>
2013-06-11 19:04:02 +0700

    Create Draw.java

---
+++ lw/src/ru/nsu/ivannikova/logoworld/commands/Draw.java
@@ -0,0 +1,16 @@
+package ru.nsu.ivannikova.logoworld.commands;
+
+import ru.nsu.ivannikova.logoworld.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 1
+ * Date: 15.04.13
+ * Time: 11:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Draw implements Command {
+    public void execute(LW lw, Point point, String parameters) {
+        if (!lw.isMode()) lw.changeMode();
+    }
+}
